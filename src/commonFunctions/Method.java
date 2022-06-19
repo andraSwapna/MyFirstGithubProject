50934118b46ac38214f6152ba3800871e783b176
Raju <Raju@192.168.0.123>
2022-06-19 10:12:34 +0530

    New method added

---
+++ src/commonFunctions/Method.java
@@ -0,0 +1,15 @@
+package commonFunctions;
+
+import org.testng.Reporter;
+
+public class Method {
+
+	public static void main()
+	{
+		int a=10;
+		int b=20;
+		int c= a+b;
+		Reporter.log("Additon oftwo numbers is::", true);		
+	}
+
+}
